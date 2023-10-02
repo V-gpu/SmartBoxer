4bc6e92fe86671d97e2d798136a1f561aade2903
Vipul Baghel <60958474+V-gpu@users.noreply.github.com>
2023-10-02 19:09:51 +0530

    Add files via upload

---
+++ SLOAN/smartboxerM1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SUBFOLD0=${PWD%%/}/00boundarydetection/boundarydetection.py
+#SUBFOLD1=${PWD%%/}/0SegmentedVideos/renamefile.py
+
+python3 $SUBFOLD0 # &&
+#python3 $SUBFOLD1
+
+
+# the next step you need to input the videos
---
+++ SLOAN/smartboxerM2.sh
@@ -0,0 +1,67 @@
+##!/bin/bash
+
+
+read -p "Enter a directory: " ${PWD}
+
+SUBFOLD0=${PWD%%/}/0SegmentedVideos/Toprocess/
+SUBFOLD1=${PWD%%/}/1detection/runs/predict-seg/*
+SUBFOLD11=${PWD%%/}/1detection/runs/predict-seg/
+SUBFOLD2=${PWD%%/}/1detection/segment/predict.py
+SUBFOLD3=${PWD%%/}/1detection/yolov7-seg.pt
+SUBFOLD5=${PWD%%/}/2Tracking/histotracker.py
+SUBFOLD6=${PWD%%/}/3Trajectory/trajectory.py
+
+rm -rf $SUBFOLD1 
+
+if [ -d "$SUBFOLD0" ]; then
+    echo "Number of files is $(find "$SUBFOLD0" -type f | wc -l)"
+	#Totalvideofiles = $(find "$SUBFOLD0" -type f | wc -l)
+    echo "Number of directories is $(find "$SUBFOLD0" -type d | wc -l)"
+else
+    echo "[ERROR]  Please provide a directory."
+    exit 1
+fi
+
+start=0
+end=$(find "$SUBFOLD0" -type f | wc -l) 
+#echo 'ENG' $end
+
+
+OIFS="$IFS"
+IFS=$'\n'
+for filename in `find "$SUBFOLD0" -type f -name "*.avi"`
+do
+	videoarr[$index]="$filename"
+	index=$(($index+1))
+done;	
+IFS="$OIFS"
+
+
+for ((i=start; i<=end-1; i++))
+do
+	echo 'NAMECHECK' # "${videoarr[i]}"
+	#python3 $SUBFOLD2 --weights $SUBFOLD3 --source "${videoarr[i]}" --class 0 --device 1 --imgsz 640 --max-det=100 
+done
+
+
+Totalfilesexp="$(ls ${PWD%%/}/1detection/runs/predict-seg/)"
+
+for filenameexp in ${Totalfilesexp}
+do
+
+	SUBFOLD44=${SUBFOLD11}"${filenameexp}"
+	exparr[$index1]=$SUBFOLD44
+	index1=$(($index1+1))
+done;
+
+
+for ((i=start; i<=end-1; i++))
+do
+	# echo "${exparr[i]}"
+	# echo "${videoarr[i]}"
+	python3 $SUBFOLD5 --fullpathpickle "${exparr[i]}" --filelist "${videoarr[i]}" 
+	#sleep 5
+	python3 $SUBFOLD6 --filelist "${videoarr[i]}"
+done
+
+
