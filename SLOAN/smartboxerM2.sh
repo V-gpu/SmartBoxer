32001eaf83c0719e18e358629c09d007d3495484
Vipul Baghel <60958474+V-gpu@users.noreply.github.com>
2023-11-29 20:09:35 +0530

    Add files via upload

--- SLOAN/smartboxerM2.sh
+++ SLOAN/smartboxerM2.sh
@@ -11,7 +11,7 @@ SUBFOLD3=${PWD%%/}/1detection/yolov7-seg.pt
 SUBFOLD5=${PWD%%/}/2Tracking/histotracker.py
 SUBFOLD6=${PWD%%/}/3Trajectory/trajectory.py
 
-rm -rf $SUBFOLD1 
+#rm -rf $SUBFOLD1 
 
 if [ -d "$SUBFOLD0" ]; then
     echo "Number of files is $(find "$SUBFOLD0" -type f | wc -l)"
@@ -29,7 +29,8 @@ end=$(find "$SUBFOLD0" -type f | wc -l)
 
 OIFS="$IFS"
 IFS=$'\n'
-for filename in `find "$SUBFOLD0" -type f -name "*.avi"`
+#for filename in `find "$SUBFOLD0" -type f -name "*.avi"`
+for filename in "$SUBFOLD0"/*
 do
 	videoarr[$index]="$filename"
 	index=$(($index+1))
@@ -39,27 +40,27 @@ IFS="$OIFS"
 
 for ((i=start; i<=end-1; i++))
 do
-	echo 'NAMECHECK' # "${videoarr[i]}"
-	#python3 $SUBFOLD2 --weights $SUBFOLD3 --source "${videoarr[i]}" --class 0 --device 1 --imgsz 640 --max-det=100 
+	echo 'NAMECHECK', "${videoarr[i]}"
+	#python3 $SUBFOLD2 --weights $SUBFOLD3 --source "${videoarr[i]}" --class 0 --imgsz 640 --max-det=100 --iou-thres=0.7 --conf-thres=0.7
 done
 
 
 Totalfilesexp="$(ls ${PWD%%/}/1detection/runs/predict-seg/)"
 
-for filenameexp in ${Totalfilesexp}
+for filenameexp in "$SUBFOLD11"/*
 do
-
-	SUBFOLD44=${SUBFOLD11}"${filenameexp}"
+	SUBFOLD44="${filenameexp}"
 	exparr[$index1]=$SUBFOLD44
 	index1=$(($index1+1))
+	echo 'check', $index1
 done;
 
 
 for ((i=start; i<=end-1; i++))
 do
-	# echo "${exparr[i]}"
-	# echo "${videoarr[i]}"
-	python3 $SUBFOLD5 --fullpathpickle "${exparr[i]}" --filelist "${videoarr[i]}" 
+	echo '12', "${exparr[i]}"
+	echo '123', "${videoarr[i]}"
+	#python3 $SUBFOLD5 --fullpathpickle "${exparr[i]}" --filelist "${videoarr[i]}" 
 	#sleep 5
 	python3 $SUBFOLD6 --filelist "${videoarr[i]}"
 done
